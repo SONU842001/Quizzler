ac28fab8395de5af57a3185bf164630d3fc4da1f
SONU842001 <54150646+SONU842001@users.noreply.github.com>
2022-05-28 03:30:22 +0530

    Completed upto wrong or right answer

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -9,12 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var questionLabel: UILabel!
     
+    @IBOutlet weak var barProgress: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    
+    @IBOutlet weak var falseButton: UIButton!
+    let quiz=[["Four + Two is equal to six.","True"],
+              ["Five - Three is greater tahn one.","False"]
+              ,["Three + eight is equal to eleven.","True"]]
+    var questionNumber = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        updateUI()
+        
     }
-
+    @IBAction func answerButtonPressed(_ sender: Any) {
+        let userAnswer = (sender as AnyObject).currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        if userAnswer == actualAnswer {
+            print("Right")
+            
+        }
+        else
+        {
+            print("Wrong")
+        }
+        if questionNumber + 1  < quiz.count{
+        questionNumber += 1
+        updateUI()
+        }
+        else {
+            
+            trueButton.isHidden = true
+            falseButton.isHidden = true
+            questionComlete()
+        }
+            
+    }
+    func updateUI(){
+        questionLabel.text = quiz[questionNumber][0]
+    }
+    func questionComlete(){
+        questionLabel.text = "You completed your test."
+    }
 
 }
 
